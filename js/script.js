1fed69a110583dfd595764e05b29094d7b91bdb1
JulyAnAr88 <july.an.ar88@gmail.com>
2024-05-20 01:14:44 -0300

    fix styles

--- js/script.js
+++ js/script.js
@@ -54,36 +54,6 @@
   });
 })(document);
 
-/* *********Carrusel ********** */
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-} 
-
 /* ********** Tyiping effect ********** */
 const $text = document.querySelector(".hero-pretitle-name");
 let str = $text.innerHTML;
@@ -106,3 +76,4 @@ function typing() {
 }
 
 setTimeout(typing, 1500);
+
